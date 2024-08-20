64db273555347286639eafc015873c6827c020b9
jidonglei <jidl@tuya.com>
2024-08-20 09:29:22 +0800

    update

--- pytorch/train.py
+++ pytorch/train.py
@@ -166,6 +166,6 @@ if __name__ == '__main__':
     args.checkpoint_dir = 'checkpoint/' + args.keyname
     args.test_dir = 'test/' + args.keyname
 
-    print('keyname=%s task=%s started'%(args.keyname, args.task))
+    print('keyname=%s task=%s checkpoint=%s restore=%s started'%(args.keyname, args.task, args.checkpoint_dir, args.restore))
 
     main(args)
